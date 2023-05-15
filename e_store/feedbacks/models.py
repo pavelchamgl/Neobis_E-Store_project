e4452e055b3aa1e9aaf4d15c94f264a197f15de8
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-05-15 10:04:27 +0300

    Added Comment and Rating models

--- e_store/feedbacks/models.py
+++ e_store/feedbacks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+from users.models import User
+
+
+class Rating(models.Model):
+    rate_choice = (
+        ('0', ''),
+        ('1', '*'),
+        ('2', '**'),
+        ('3', '***'),
+        ('4', '****'),
+        ('5', '*****'),
+
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    rate = models.CharField(max_length=5, choices=rate_choice, default='')
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=250)
+    date_create = models.DateTimeField(auto_now_add=True)
